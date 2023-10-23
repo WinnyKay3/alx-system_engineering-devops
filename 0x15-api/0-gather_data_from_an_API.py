162fec6df804f487ef503c2ee01182acf2df2085
WinnyKay3 <winffredkalondu@gmail.com>
2023-10-23 14:34:40 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
     employee_name = user_data.get("name")
 
     # fetches todo list data
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}"\
-        .format(employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id
+    )
     todo_response = requests.get(todo_url)
     todo_data = todo_response.json()
 
@@ -25,8 +26,11 @@ if __name__ == "__main__":
     completed_tasks = sum(1 for task in todo_data if task.get("completed"))
 
     # Displays the result
-    print("Employee {} is done with tasks({}/{}):"\
-          .format(employee_name, completed_tasks, total_tasks))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, completed_tasks, total_tasks
+        )
+    )
     for task in todo_data:
         if task.get("completed"):
             print("\t {}".format(task.get("title")))
