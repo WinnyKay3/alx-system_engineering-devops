3491862efa2bb119303eadab28356366ea6269d5
WinnyKay3 <winffredkalondu@gmail.com>
2023-10-23 17:01:19 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
 
     employee_id = sys.argv[1]
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
     user_response = requests.get(user_url)
     user_data = user_response.json()
     employee_name = user_data.get("username")
