dbb07acb9a4c57a4ba0255369d4e38f0327033a3
WinnyKay3 <winffredkalondu@gmail.com>
2023-11-14 22:10:08 +0000

    0x18 Webstack Monitoring README.md:

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # fix bad phpp extension to php in wordpress file
 
 exec { 'fix-extension':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php',
 }
