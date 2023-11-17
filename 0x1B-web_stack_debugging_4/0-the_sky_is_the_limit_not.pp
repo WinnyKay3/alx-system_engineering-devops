f7b39607a415bf6e40f241c66bc8d5805a82c74d
WinnyKay3 <winffredkalondu@gmail.com>
2023-11-17 16:02:13 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # enables nginx to handle more incoming traffic
 # Increases users limit
 exec { 'add-user-limit':
-  command => 'sed -i "s/15/4000/" /etc/default/nginx',
+  command => 'sed -i "s/15/4094/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin'
 }
 
